7e89446143bbf982e89012359ce269ce44eeed0d
Artur Bartkevic <artur.bartkevic@gmail.com>
2015-03-08 14:01:33 +0200

    Remove pyc; Add to gitignore

--- src/app/settings.py
+++ src/app/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',
     'modules.reservation'
 )
 
@@ -60,8 +59,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django_db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'django',
+        'PASSWORD': 'django2235',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
@@ -84,4 +88,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-ACCOUNT_ACTIVATION_DAYS=7
+LOGIN_URL = '/accounts/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
